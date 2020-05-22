ef35d91397368dec4b61397b534b5f8065770da8
Vojtech Helmer <dwyanewade@email.cz>
2020-05-22 23:14:43 +0200

    Process Psr7ServerRequest with UrlScript

--- src/ApiPresenter.php
+++ src/ApiPresenter.php
@@ -6,6 +6,7 @@ use Apitte\Core\Application\IApplication;
 use Apitte\Core\Http\ApiRequest;
 use Contributte\Psr7\Psr7ServerRequestFactory;
 use Contributte\Psr7\Psr7Uri;
+use Contributte\Psr7\Psr7UriFactory;
 use Nette\Application\IPresenter;
 use Nette\Application\IResponse;
 use Nette\Application\Request;
@@ -30,10 +31,9 @@ class ApiPresenter implements IPresenter
 	public function run(Request $request): IResponse
 	{
 		$url = $this->request->getUrl();
-		$url = substr($url->getPath(), strlen($url->getScriptPath()));
 
 		$psrRequest = Psr7ServerRequestFactory::fromNette($this->request)
-			->withUri(new Psr7Uri($url));
+			->withUri(Psr7UriFactory::fromNette($url));
 		$psrRequest = new ApiRequest($psrRequest);
 
 		return new CallbackResponse(function () use ($psrRequest): void {
